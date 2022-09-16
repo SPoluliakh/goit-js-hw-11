ea40e554bec31837d1e84e2e8baa6219ee567998
SPoluliakh <S_Poluliakh@ukr.net>
2022-09-16 22:24:31 +0300

    finish draft version

--- src/index.js
+++ src/index.js
@@ -1,29 +1,57 @@
 import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
 
-const refs = {
-  formEl: document.querySelector('form#search-form'),
-  inputEl: document.querySelector('input[type="text"]'),
-  btnEl: document.querySelector('button[type="submit"]'),
-  murkupEl: document.querySelector('.gallery'),
-};
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import refs from './js/refs.js';
+
+const gallerySimpleLightbox = new SimpleLightbox('.gallery a');
+
+let page = 1;
+let searchedData = '';
 
 refs.formEl.addEventListener('submit', onFormEl);
-let page = 1;
+refs.btnEl.addEventListener('click', onBtnEl);
+
+refs.inputEl.addEventListener('input', evt => {
+  searchedData = evt.target.value;
+});
 
 function onFormEl(evt) {
   evt.preventDefault();
-  const searchedFoto = refs.formEl.elements.searchQuery.value;
-
-  fetchInfo(searchedFoto)
+  refs.murkupEl.innerHTML = '';
+  refs.btnEl.classList.add('is-hidden');
+  fetchInfo(searchedData)
     .then(data => {
       if (!data.hits.length) {
         return Notiflix.Notify.failure(
           'Sorry, there are no images matching your search query. Please try again.'
         );
       }
+      Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+
+      refs.btnEl.classList.remove('is-hidden');
+      page += 1;
+
+      addMurkup(refs.murkupEl, data.hits);
+      gallerySimpleLightbox.refresh();
+    })
+    .catch(err => console.log(err));
+}
+
+function onBtnEl() {
+  fetchInfo(searchedData)
+    .then(data => {
+      if (!data.hits.length) {
+        return Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      }
+      refs.btnEl.classList.remove('is-hidden');
       page += 1;
       console.log(data);
-      return addMurkup(refs.murkupEl, data.hits);
+      addMurkup(refs.murkupEl, data.hits);
+      gallerySimpleLightbox.refresh();
     })
     .catch(err => console.log(err));
 }
@@ -33,7 +61,7 @@ function fetchInfo(element) {
 
   const searchParams = new URLSearchParams({
     key: API_KEY,
-    q: element,
+    q: ` ${element}`,
     image_type: 'photo',
     orientation: 'horizontal',
     safesearch: 'true',
@@ -44,39 +72,49 @@ function fetchInfo(element) {
 
   return fetch(url).then(res => {
     if (res.status === 400) {
+      refs.btnEl.classList.add('is-hidden');
       return Notiflix.Notify.info(
         "We're sorry, but you've reached the end of search results."
       );
+    } else if (res.status === 404) {
+      return [];
     }
     return res.json();
   });
 }
 
 function createMurkup(elements) {
+  console.log(elements, 'images');
   const murkup = elements
     .map(element => {
+      console.log(element, 'im');
       return `<div class="photo-card">
-  <img src="${element.webformatURL}" alt="${element.tags}" loading="lazy" />
-  <div class="info">
+  
+   <a href="${element.largeImageURL}"><img class="photo" src="${element.webformatURL}" alt="${element.tags}" title="${element.tags}" loading="lazy"/></a>
+  
+   <div class="info">
     <p class="info-item">
-      <b>Likes</b> ${element.likes}
+      <b>Likes</b><span> ${element.likes}</span>
     </p>
     <p class="info-item">
-      <b>Views</b> ${element.views}
+      <b>Views</b><span> ${element.views}</span>
     </p>
     <p class="info-item">
-      <b>Comments</b> ${element.comments}
+      <b>Comments</b><span> ${element.comments}</span>
     </p>
     <p class="info-item">
-      <b>Downloads</b> ${element.downloads}
+      <b>Downloads</b><span> ${element.downloads}</span>
     </p>
+    
   </div>
-</div> `;
+</div>`;
     })
     .join('');
+
   return murkup;
 }
 
 function addMurkup(div, elements) {
-  div.innerHTML = createMurkup(elements);
+  const murkup = createMurkup(elements);
+  div.insertAdjacentHTML('beforeend', murkup);
 }
