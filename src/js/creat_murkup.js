0beb2668da55574505b5d910276e3505778097f7
SPoluliakh <S_Poluliakh@ukr.net>
2022-09-20 13:07:57 +0300

    updated

--- src/js/creat_murkup.js
+++ src/js/creat_murkup.js
@@ -13,10 +13,18 @@ export default function createMurkup(elements) {
   /></a>
 
   <div class="info">
-    <p class="info-item"><b>Likes</b><span> ${element.likes}</span></p>
-    <p class="info-item"><b>Views</b><span> ${element.views}</span></p>
-    <p class="info-item"><b>Comments</b><span> ${element.comments}</span></p>
-    <p class="info-item"><b>Downloads</b><span> ${element.downloads}</span></p>
+    <p class="info-item"><b>Likes</b><span> ${element.likes
+      .toString()
+      .replace(/\B(?=(\d{3})+(?!\d))/g, ' ')}</span></p>
+    <p class="info-item"><b>Views</b><span> ${element.views
+      .toString()
+      .replace(/\B(?=(\d{3})+(?!\d))/g, ' ')}</span></p>
+    <p class="info-item"><b>Comments</b><span> ${element.comments
+      .toString()
+      .replace(/\B(?=(\d{3})+(?!\d))/g, ' ')}</span></p>
+    <p class="info-item"><b>Downloads</b><span> ${element.downloads
+      .toString()
+      .replace(/\B(?=(\d{3})+(?!\d))/g, ' ')}</span></p>
   </div>
 </div>
 `;
